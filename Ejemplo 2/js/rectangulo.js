46c7a0d56fb640b390e194055f276af1464964d7
José <josemejia61976751@gmail.com>
2023-08-09 02:50:33 -0600

    Se agregan los archivos

---
+++ Ejemplo 2/js/rectangulo.js
@@ -0,0 +1,5 @@
+var base = prompt("Introduzca la base del rectangulo:", "");
+var altura = prompt("Introduzca la altura del rectangulo:", "");
+var area;
+area = base*altura;
+document.write("<header><h1>El área del rectangulo es " + area + "</h1><hr/><br/></header>");
---
+++ Ejemplo 2/js/triangulo.js
@@ -0,0 +1,6 @@
+var base = prompt("Introduzca la base del triangulo:", "");
+var altura = prompt("Introduzca la altura del triangulo:", "");
+const divisor = 2;
+var area;
+area = base*altura/divisor;
+document.write("<header><h1>El área del triangulo es " + area + "</h1><hr/><br/></header>");
